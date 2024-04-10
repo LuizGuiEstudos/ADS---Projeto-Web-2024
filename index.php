fcf0e9bd0901aeb2ec62117f34811e139a3085d9
LuizGuiEstudos <157849941+LuizGuiEstudos@users.noreply.github.com>
2024-04-10 20:30:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+    $autor = "Luiz Guilherme";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Web - 2024</title>
+    <link href="./css/boot.css" rel="stylesheet" type="text/css">
+    <link href="./css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+
+    <header> 
+
+        <div> 
+            <!-- td - inserir logo -->
+        </div>
+        <nav>
+            <ul>
+                    <li> <a href="#">Quem Somos</a> </li>
+                    <li> <a href="#">Tutoriais</a> </li>
+                    <li> <a href="#">Cursos</a> </li>
+                    <li> <a href="#">Contato</a> </li>
+
+            </ul>    
+        </nav>
+        
+    </header>
+
+
+    <section>
+
+        <header>
+            <h1> Quem Somos </h1>
+        </header>
+        <div>
+            Texto do Quem Somos
+        </div>
+
+    </section>
+
+
+    <section>
+
+        <header>
+            <h1> Tutoriais </h1>
+            <p> Aqui você encontrará alguns tutoriais abordados nos cursos! Aproveite!!! </p>
+        </header>
+
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2>Front-End</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2>Back-End</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2>Banco de Dados</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2>Designer</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+
+     </section>
+
+
+     <section>
+
+        <header>
+            <h1> Cursos </h1>
+            <p> Aqui você encontrará alguns cursos incríveis!!! </p>
+        </header>
+
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2> HTML5 </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2> PHP </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2> React </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+        <article>
+            <a href="#"> <img src="#" alt="" title=""> </a>
+            <h2> Angular </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet feugiat diam, et pellentesque est fermentum sit amet. Nulla convallis metus non molestie sagittis. In pharetra sem euismod aliquet fringilla. Praesent pharetra scelerisque volutpat. Donec placerat augue urna, et tincidunt sapien posuere non. Curabitur feugiat laoreet mauris eget vehicula. Morbi imperdiet elit metus, non iaculis leo egestas a.</p>
+        </article>
+
+     </section>
+
+
+     <section>
+
+        <header>
+            <h2> Entre contato com a nossa equipe para receber propostas de cursos </h2>
+            <form action="#" method="POST">
+                <input type="text" name="nome" placeholder="Informe seu nome" required/><span>*</span>
+                <input type="email" name="email" placeholder="Informe seu email" required/><span>*</span>
+                <input type="submit" value="enviar"/>
+            </form>
+        </header>
+        
+
+     </section>
+
+
+    <footer>
+
+        <div>
+            <ul>
+                    <li> <a href="#">Quem Somos</a> </li>
+                    <li> <a href="#">Tutoriais</a> </li>
+                    <li> <a href="#">Cursos</a> </li>
+                    <li> <a href="#">Contato</a> </li>
+
+            </ul> 
+        </div>
+        <div> Desenvolvido por: <?=$autor;?> </div>
+        <div> Redes Sociais </div>
+        <a href="#"> <img src="./img/instagram.svg"/> </a>
+        <a href="#"> <img src="./img/facebook.svg"/> </a>
+        <a href="#"> <img src="./img/x-twitter.svg"/> </a>
+        <a href="#"> <img src="./img/youtube.svg"/> </a>
+
+    </footer>
+
+
+</body>
+</html>
